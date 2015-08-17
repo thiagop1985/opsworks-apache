9ce2adc94ff108c88a35d695558319d722c05def
Thiago Paulino <thiagop1985@gmail.com>
2015-08-17 17:56:20 -0300

    Update default.rb

--- aus/recipes/default.rb
+++ aus/recipes/default.rb
@@ -1,10 +1,3 @@
-# Install some stuff.
-["mysql-client"].each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
 # Replace php.ini file.
 template '/etc/php5/apache2/php.ini' do
   source "php.ini.erb"
