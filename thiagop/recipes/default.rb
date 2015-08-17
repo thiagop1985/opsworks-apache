9df7e430d6c556b8e0390c3a50cc96546635458d
Paulino <thiagop@ac87a312e2da.ant.amazon.com>
2015-08-17 18:48:26 -0300

    sdsd

--- thiagop/recipes/default.rb
+++ thiagop/recipes/default.rb
@@ -1,15 +1,15 @@
 # Replace php.ini file.
 template '/etc/php5/apache2/php.ini' do
-  source "php.ini"
+  source "php.ini.erb"
 end
 
 # Replace apache2.conf file.
 template '/etc/apache2/apache2.conf' do
-  source "apache2.conf"
+  source "apache2.conf.erb"
 end
 
 # Replace limits.conf file.
 template '/etc/security/limits.conf' do
-  source "limits.conf"
+  source "limits.conf.erb"
 end
 
