286bacdcd337df232ad04106b45c9b508799b93c
Paulino <thiagop@ac87a312e2da.ant.amazon.com>
2015-08-19 17:02:31 -0300

    commit

---
+++ opsworks-demo/recipes/default.rb
@@ -0,0 +1,18 @@
+# Replace php.ini file.
+template '/etc/php5/apache2/php.ini' do
+  source "php.ini.erb"
+end
+
+# Replace apache2.conf file.
+template '/etc/apache2/apache2.conf' do
+  source "apache2.conf.erb"
+end
+
+# Replace limits.conf file.
+template '/etc/security/limits.conf' do
+  source "limits.conf.erb"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
